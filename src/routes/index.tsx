8e190ed709c90faf5b76057507aaea1b26d4d5a3
Kernal <194436535+MrLogistick@users.noreply.github.com>
2026-06-24 10:43:01 +0930

    Title Scaffold

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,46 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: Home });
 
-export const UIElement = () => {
-  return <div>Hello</div>;
+const Button = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <button className="px-1 text-2xl font-bold">
+      {children}
+    </button>
+  )
+}
+
+const SideBar = () => {
+  return (
+    <div className="w-1/3 border text-center">
+      pic or wtv
+    </div>
+  )
+};
+
+const Title = () => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full border">
+      <div className="text-center mb-20">
+        <h1 className="text-8xl font-bold">Title</h1>
+        <h2 className="text-2xl font-semibold">Subtitle</h2>
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <Button>Start</Button>
+        <Button>Stats</Button>
+        <Button>Levels</Button>
+        <Button>Skills</Button>
+        <Button>Credits</Button>
+      </div>
+    </div>
+  )
 };
 
 function Home() {
   return (
     <>
-      <div className="flex flex-row space-x-3 h-svh mx-1">
-        <div className="w-1/3 border">hello</div>
-        <div className="max-w-full border grow">Hello</div>
+      <div className="flex space-x-3 h-svh mx-1">
+        <SideBar/>
+        <Title/>
       </div>
     </>
   );
