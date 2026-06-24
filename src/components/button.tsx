007ad98c6ba4815f9f3b3f0dd9e32f0574c7dbdf
Kernal <194436535+MrLogistick@users.noreply.github.com>
2026-06-24 15:30:25 +0930

    AndTwo

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -19,3 +19,23 @@ export const Button = ({
     </button>
   );
 };
+
+export const TitleButton = ({
+  children,
+  className,
+  onClick
+}: {
+  children: React.ReactNode,
+  className?: string;
+  onClick?: () => void;
+}) => {
+  return (
+    <button
+      type="button"
+      className={`h-12 w-48 text-2xl font-bold bg-[#964B00] rounded-full ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { TitleButton } from "../components/button"
 
 export const Route = createFileRoute("/")({ component: Home });
 
@@ -12,25 +13,27 @@ const Button = ({ children }: { children: React.ReactNode }) => {
 
 const SideBar = () => {
   return (
-    <div className="w-1/3 border-r text-center">
-      pic or wtv
+    <div className="w-1/3 text-center z-0">
+      <div className="rounded-full aspect-square w-2/1 -translate-x-1/3 -translate-y-1/2 bg-[#006472]">
+      </div>
     </div>
   )
 };
 
+// const gradientClass = "bg-linear-to-l from-[#49C5B1] to-[#002858]"
+// bg-linear-to-l to-[#49C5B1] from-[#011A38] from-72% to-80%
 const Title = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-full">
+    <div className="flex flex-col justify-center items-center w-full z-20 text-white bg-linear-to-l from-[#0D006C] to-transparent from-90%">
       <div className="text-center mb-20">
         <h1 className="text-8xl font-bold">Title</h1>
         <h2 className="text-2xl font-semibold">Subtitle</h2>
       </div>
-      <div className="flex flex-col items-center gap-2">
-        <Button>Start</Button>
-        <Button>Stats</Button>
-        <Button>Levels</Button>
-        <Button>Skills</Button>
-        <Button>Credits</Button>
+      <div className="flex flex-col items-center gap-4">
+        <TitleButton>Start</TitleButton>
+        <TitleButton>Level Select</TitleButton>
+        <TitleButton>Statistics</TitleButton>
+        <TitleButton>Skills</TitleButton>
       </div>
     </div>
   )
@@ -39,7 +42,7 @@ const Title = () => {
 function Home() {
   return (
     <>
-      <div className="flex h-svh">
+      <div className="flex h-svh bg-[#005F6C]">
         <SideBar/>
         <Title/>
       </div>
